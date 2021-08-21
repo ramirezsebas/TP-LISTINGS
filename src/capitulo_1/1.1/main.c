3953c1099bd67fbbd9eff6db4a0727e06f9f7605
Sebastian Ramirez <ramirezmatias946@gmail.com>
2021-08-21 13:16:43 -0400

    Creamos el main.c del Listing 1.1, donde inciia la ejecucion del programa Reciprocal.

---
+++ src/capitulo_1/1.1/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "reciprocal.hpp"
+
+int main(int argc, char const *argv[])
+{
+    int i;
+    i = atoi(argv[1]);
+    printf("The reciprocal of %d is %g\n", i, reciprocal(i));
+    return 0;
+}
